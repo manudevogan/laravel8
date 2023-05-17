2ff6cce0b93bd2f63e613cddd8b2c9533341aca6
Manu Aimee <manudevogan@gmail.com>
2023-05-17 14:12:36 +0000

    Add welcome message

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+    </head>
+    <body>
+        <h1>Hello from Togo!</h1>
+        <p> on est le {{date('d/m/y')}} et il est {{date('h:i:A')}}</p>
+
+        JE FAIS UN TEST
+    </body>
+    <footer>
+
+    </footer>
+
+       
+       
+       
+</html>
